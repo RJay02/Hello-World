4e628f645978e11e3ef5e3a6120830828cd0b364
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-11-25 00:52:38 +0000

    Adding comments

--- ALL HOMEWORK IS HERE ED/Chapter 4 Exercises/Question 3 Age Checker.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 4 Exercises/Question 3 Age Checker.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    //Check if user enters valid input
+int main() {//Main function
+    //Prompt in to ask the user about their age
     cout << "Enter your age:";
+    //Declaring variables 
     int age;
     cin >> age;
+    //While loop if the age entered is not valid
     while (cin.fail()) {
-        cout << "Invalid input" << endl;
+        cout << "Invalid input" << endl;//output statement if the enetered age is incorrect 
         cin.clear(); //clear bad input flag
         cin.ignore(1000, '\n'); //ignore input
         cout << "Enter a valid age:";
-        cin >> age; //get number again
+        cin >> age; //get new age
     }
-    cout << "The entered age is:" << age << endl;
+    cout << "The entered age is:" << age << endl;//Output statement showing the age entered 
 
-    return 0;
+    return 0;//Return statement
 }
