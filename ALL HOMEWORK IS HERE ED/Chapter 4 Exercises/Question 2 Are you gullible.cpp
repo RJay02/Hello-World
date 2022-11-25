3f5b1e9bce386110bcfd9060ff80110e459c4a9c
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-11-25 00:49:11 +0000

    Adding comments

--- ALL HOMEWORK IS HERE ED/Chapter 4 Exercises/Question 2 Are you gullible.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 4 Exercises/Question 2 Are you gullible.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    while (true) {
-        int num;
-        cout << "Please enter a number other than 5:" << endl;
+int main() { //Main function 
+    while (true) { //While conditional check
+        int num;//Declaring the variables
+        cout << "Please enter a number other than 5:" << endl; //Prompt in to get a number
         cin >> num;
-        if (num == 5) {
-            cout << "Hey! you weren't supposed to enter 5!" << endl;
-            break;
+        if (num == 5) { // If statement to stop when the user enters 5 
+            cout << "Hey! you weren't supposed to enter 5!" << endl; //Output statement if the user enters 5
+            break;//Beaking the while loop 
         }
     }
 }
