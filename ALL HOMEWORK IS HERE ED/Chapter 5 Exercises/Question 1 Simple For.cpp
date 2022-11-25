aedd4e6e2ee308d99aaf00fc4542e553763a1142
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-11-25 00:55:09 +0000

    Adding more comments

--- ALL HOMEWORK IS HERE ED/Chapter 5 Exercises/Question 1 Simple For.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 5 Exercises/Question 1 Simple For.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() {//Main function 
+	//Using for loop to iterate through the condition until satisfied 
 	for (int i = 0; i < 10; i++) {
-		cout << "Hello World" << endl;
+		cout << "Hello World" << endl;//Print statement 
 	}
 }
