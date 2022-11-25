5badae7c7c3ad45040a27ab1158a8b180d5b78fe
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-11-25 00:44:53 +0000

    Solving question 1 exercise 4

---
+++ ALL HOMEWORK IS HERE ED/Chapter 4 Exercises/Question 1 Reverse 9 Times table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 108; //declare and initalise variable
+
+    while (num > 0) { //while conditional check
+        //code to output then decrease number
+        cout << num << endl;
+        num -= 9;
+    }
+    return 0;//return statement 
+}
