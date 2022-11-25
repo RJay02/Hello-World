5759840eb753c350c27785f07e16f8893c8f1f5f
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-11-25 00:37:53 +0000

    Adding comments

--- ALL HOMEWORK IS HERE ED/Chapter 3 Exercises/Question 2 Days of the month.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 3 Exercises/Question 2 Days of the month.cpp
@@ -1,15 +1,17 @@
 #include <iostream> //Pre-processor directive to include the iostream file for inputs and outputs
 using namespace std; //Used when a variable has a string 
 int main() { //Main Function 
+    //Initialising the variables 
     int x;
     int i = 0;
-    while (i == 0) {
-        cout << "Please enter a number for you month:";
+    while (i == 0) { //while loop to keep on repeating the question
+        cout << "Please enter a number for you month:";//Prompt in and ask the user about the month 
         cin >> x;
-        switch (x) {
+        switch (x) { //Using switch statement describing all the cases 
+            // Different cases for each month of the year 
         case 1:
-            cout << "January has 31 days" << endl;
-            break;
+            cout << "January has 31 days" << endl;//Output statement
+            break;//Break statement 
         case 2:
             cout << "Febuary has 28 days" << endl;
             break;
@@ -43,7 +45,7 @@ int main() { //Main Function
         case 12:
             cout << "December has 31 days" << endl;
             break;
-        default:
+        default: // Justifying the default case 
             cout << "Invalid input please enter a number" << endl;
         }
     }
