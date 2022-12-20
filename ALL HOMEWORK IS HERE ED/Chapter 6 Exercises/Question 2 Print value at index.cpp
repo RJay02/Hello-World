376e4f443eab9ce2d7e024ab1ba4c6b5815206bf
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-12-20 18:27:08 +0000

    Create Question 2 Print value at index.cpp

---
+++ ALL HOMEWORK IS HERE ED/Chapter 6 Exercises/Question 2 Print value at index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string vehicles[] = { "Car","Train","Tractor","Skiff","Tank" };
+    cout << vehicles[3] << endl;
+    cout << vehicles[0] << endl;
+    cout << vehicles[2] << " " << vehicles[4] << endl;
+    cout << vehicles[0] << " " << vehicles[3] << " " << vehicles[1] << endl;
+
+
+    return 0;
+}
