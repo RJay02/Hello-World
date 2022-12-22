93f8c6cacc94a1ca7077a51275ecdc85cc7799c2
RJay02 <115995680+RJay02@users.noreply.github.com>
2022-12-22 20:43:56 +0000

    Update Question 1 Simple For.cpp

--- ALL HOMEWORK IS HERE ED/Chapter 5 Exercises/Question 1 Simple For.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 5 Exercises/Question 1 Simple For.cpp
@@ -5,5 +5,5 @@ int main() {//Main function
 	//Using for loop to iterate through the condition until satisfied 
 	for (int i = 0; i < 10; i++) {
 		cout << "Hello World" << endl;//Print statement 
-	}
+	l
 }
