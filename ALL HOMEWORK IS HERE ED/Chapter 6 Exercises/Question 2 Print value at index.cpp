2a4df614b29287910a60d37b4c5c8d74bc54c480
RJay02 <115995680+RJay02@users.noreply.github.com>
2023-01-09 12:52:53 +0000

    Update Question 2 Print value at index.cpp

--- ALL HOMEWORK IS HERE ED/Chapter 6 Exercises/Question 2 Print value at index.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 6 Exercises/Question 2 Print value at index.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-using namespace std;
+#include <iostream>// Pre-processor directive to include the iostream file for inputs and outputs
+using namespace std;// Declares the use of standard namespace
 
-int main() {
-    string vehicles[] = { "Car","Train","Tractor","Skiff","Tank" };
-    cout << vehicles[3] << endl;
-    cout << vehicles[0] << endl;
-    cout << vehicles[2] << " " << vehicles[4] << endl;
-    cout << vehicles[0] << " " << vehicles[3] << " " << vehicles[1] << endl;
+int main() { //Main Function 
+    string vehicles[] = { "Car","Train","Tractor","Skiff","Tank" }; //Declaring the array  
+    cout << vehicles[3] << endl; //Outputting the index 3 to console
+    cout << vehicles[0] << endl; //Outputting the index 0 to console
+    cout << vehicles[2] << " " << vehicles[4] << endl; //Outputting “Tractor Tank” to console using array indexes
+    cout << vehicles[0] << " " << vehicles[3] << " " << vehicles[1] << endl; //Outputting “Car Skiff Train” to console using array indexes
 
 
-    return 0;
+    return 0; // Return Statement 
 }
