81733d1a713d06cc7022a8755e03e0c090b49680
RJay02 <115995680+RJay02@users.noreply.github.com>
2023-01-09 12:56:59 +0000

    Update Question 3 Fill it up.cpp

--- ALL HOMEWORK IS HERE ED/Chapter 6 Exercises/Question 3 Fill it up.cpp
+++ ALL HOMEWORK IS HERE ED/Chapter 6 Exercises/Question 3 Fill it up.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <iostream>// Pre-processor directive to include the iostream file for inputs and outputs
+using namespace std;// Declares the use of standard namespace
 
-int main() {
-    int array[11];
-    for (int i = 1; i < 11; i++) {
-        cout << "Enter value " << i << " please:";
-        cin >> array[i];
+int main() {//Main function 
+    int array[11];// Declaring the array that can store up to 10 values
+    for (int i = 1; i < 11; i++) { // For loop to allow the user to enter the different values
+        cout << "Enter value " << i << " please:"; //Output statement to enter value
+        cin >> array[i]; //Prompt in to ask for values
     }
-    for (int i = 1; i < 11; i++) {
-        cout << array[i] << endl;
+    for (int i = 1; i < 11; i++) { //For loop to print the entered values  
+        cout << array[i] << endl; //Outputting the enetered values in the array 
     }
-    return 0;
+    return 0; //Return statement
 }
